c8cd18aadc05cd2dde7fc9bd9cddb8dd2ac8a373
wUssopp <122217238+wUssopp@users.noreply.github.com>
2023-02-13 13:41:25 +0100

    Update script.js

--- script.js
+++ script.js
@@ -13,11 +13,11 @@ const updateUI = function (value) {
 };
 
 btnCalc.addEventListener(`click`, function () {
-  if (input.value > 50 && input.value < 350) {
+  if (input.value > 50 && input.value <= 250) {
     updateUI(0.15);
-  } else if (input.value < 50 && input.value > 0) {
-    updateUI(0.3);
-  } else if (input.value > 350) {
+  } else if (input.value <= 50 && input.value > 0) {
+    updateUI(0.2);
+  } else if (input.value > 250) {
     updateUI(0.1);
   } else if (input.value <= 0) {
     alert(`Invalid number!`);
